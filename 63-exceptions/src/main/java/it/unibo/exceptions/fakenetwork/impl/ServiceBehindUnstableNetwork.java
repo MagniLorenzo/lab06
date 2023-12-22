7c890526fe38b18dc1f4343c7f39ade200f2ad22
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2023-12-22 16:52:40 +0100

    Esercizio 63: Risolto quarto punto

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -59,15 +59,17 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
             commandQueue.add(data);
         } else {
             final var message = data + " is not a valid keyword (allowed: " + KEYWORDS + "), nor is a number";
-            System.out.println(message);
-            commandQueue.clear();
             /*
+             * System.out.println(message);
+             * commandQueue.clear();
+             * /*
              * This method, in this point, should throw an IllegalStateException.
              * Its cause, however, is the previous NumberFormatException.
              * Always preserve the original stacktrace!
              *
              * The previous exceptions must be set as the cause of the new exception
              */
+            throw new IllegalStateException(message, exceptionWhenParsedAsNumber);
         }
     }
 
