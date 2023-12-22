0273348a07556e88d63314225192718cdf99cac7
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2023-12-22 19:23:56 +0100

    Esercizio 63: Risolto quinto punto

--- 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
@@ -56,36 +56,43 @@ public final class ArithmeticService {
      */
     public String process() {
         if (commandQueue.isEmpty()) {
-            System.out.println("No commands sent, no result available");
+            // System.out.println("No commands sent, no result available");
+            throw new IllegalStateException("No commands sent, no result available");
         }
         while (commandQueue.size() != 1) {
             final var nextMultiplication = commandQueue.indexOf(TIMES);
             final var nextDivision = commandQueue.indexOf(DIVIDED);
             final var nextPriorityOp = nextMultiplication >= 0 && nextDivision >= 0
-                ? min(nextMultiplication, nextDivision)
-                : max(nextMultiplication, nextDivision);
+                    ? min(nextMultiplication, nextDivision)
+                    : max(nextMultiplication, nextDivision);
             if (nextPriorityOp >= 0) {
                 computeAt(nextPriorityOp);
             } else {
                 final var nextSum = commandQueue.indexOf(PLUS);
                 final var nextMinus = commandQueue.indexOf(MINUS);
                 final var nextOp = nextSum >= 0 && nextMinus >= 0
-                    ? min(nextSum, nextMinus)
-                    : max(nextSum, nextMinus);
+                        ? min(nextSum, nextMinus)
+                        : max(nextSum, nextMinus);
                 if (nextOp != -1) {
                     if (commandQueue.size() < 3) {
-                        System.out.println("Inconsistent operation: " + commandQueue);
+                        // System.out.println("Inconsistent operation: " + commandQueue);
+                        final String msg = "Inconsistent operaztion: " + commandQueue;
+                        throw new IllegalStateException(msg);
                     }
                     computeAt(nextOp);
                 } else if (commandQueue.size() > 1) {
-                    System.out.println("Inconsistent state: " + commandQueue);
+                    // System.out.println("Inconsistent state: " + commandQueue);
+                    final String msg = "Inconsistent state: " + commandQueue;
+                    throw new IllegalStateException(msg);
                 }
             }
         }
         final var finalResult = commandQueue.get(0);
         final var possibleException = nullIfNumberOrException(finalResult);
         if (possibleException != null) {
-            System.out.println("Invalid result of operation: " + finalResult);
+            // System.out.println("Invalid result of operation: " + finalResult);
+            final String msg = "Invalid result of operation: " + finalResult;
+            throw new IllegalStateException(msg, possibleException);
         }
         return finalResult;
         /*
@@ -96,32 +103,40 @@ public final class ArithmeticService {
 
     private void computeAt(final int operatorIndex) {
         if (operatorIndex == 0) {
-            System.out.println("Illegal start of operation: " + commandQueue);
+            // System.out.println("Illegal start of operation: " + commandQueue);
+            final String msg = "Illegal start of operation: " + commandQueue;
+            throw new IllegalStateException(msg);
         }
         if (commandQueue.size() < 3) {
-            System.out.println("Not enough operands: " + commandQueue);
+            // System.out.println("Not enough operands: " + commandQueue);
+            final String msg = "Not enough operands: " + commandQueue;
+            throw new IllegalStateException(msg);
         }
         if (commandQueue.size() < operatorIndex + 1) {
-            System.out.println("Missing right operand: " + commandQueue);
+            //System.out.println("Missing right operand: " + commandQueue);
+            final String msg = "Missing right operand: " + commandQueue;
+            throw new IllegalStateException(msg);
         }
         final var rightOperand = commandQueue.remove(operatorIndex + 1);
         final var leftOperand = commandQueue.remove(operatorIndex - 1);
         if (KEYWORDS.contains(rightOperand) || KEYWORDS.contains(leftOperand)) {
-            System.out.println(
-                "Expected a number, but got " + leftOperand + " and " + rightOperand + " in " + commandQueue
-            );
+            //System.out.println("Expected a number, but got " + leftOperand + " and " + rightOperand + " in " + commandQueue);
+            final String msg = "Expected a number, but got " + leftOperand + " and " + rightOperand + " in " + commandQueue;
+            throw new IllegalStateException(msg);
         }
         final var right = parseDouble(rightOperand);
         final var left = parseDouble(leftOperand);
         final var operand = commandQueue.get(operatorIndex - 1);
-        final var result =  switch (operand) {
+        final var result = switch (operand) {
             case PLUS -> left + right;
             case MINUS -> left - right;
             case TIMES -> left * right;
             case DIVIDED -> left / right;
-            default ->  {
-                System.out.println("Unknown operand " + operand);
-                yield Double.NaN;
+            default -> {
+                //System.out.println("Unknown operand " + operand);
+                final String msg = "Unknown operand " + operand;
+                throw new IllegalStateException(msg);
+                //yield Double.NaN;
             }
         };
         commandQueue.set(operatorIndex - 1, Double.toString(result));
