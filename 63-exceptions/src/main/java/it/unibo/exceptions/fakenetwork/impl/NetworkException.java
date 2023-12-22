bcb78532e48f707e527c6387ed9b6fe15d1aab4c
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2023-12-22 16:27:06 +0100

    Esercizio 63: Creazione file NetworkException.java

---
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/NetworkException.java
@@ -0,0 +1,25 @@
+package it.unibo.exceptions.fakenetwork.impl;
+
+import java.io.IOException;
+
+public class NetworkException extends IOException {
+
+    private final String msg;
+    private static final String DEFAULT_MESSAGE = "Network error: no response";
+
+    public NetworkException() {
+        super();
+        this.msg = DEFAULT_MESSAGE;
+    }
+
+    public NetworkException(final String message) {
+        super();
+        this.msg = "Network error while sending message: " + message;
+    }
+
+    public String toString() {
+        String str = msg + "\n" + super.toString();
+        return str;
+    }
+
+}
