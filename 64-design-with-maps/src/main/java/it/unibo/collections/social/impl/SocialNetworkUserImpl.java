89abc41c679d0ca8283e788e0e165f3a20bf8f73
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-09 20:23:36 +0100

    Esercizio 64: Rimozione import superflui in SocialNetworkUserImpl

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -9,13 +9,10 @@ import it.unibo.collections.social.api.User;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
 /**
  * 
