79aa24cd4af6104f1b657ccf5ae5f8f2d7011a6e
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-10 21:54:58 +0100

    Esercizio 65: Modificato GraphImpl con aggiunta di campi e inizio implementazione getPath

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -3,17 +3,21 @@ package it.unibo.generics.graph.impl;
 import it.unibo.generics.graph.api.Graph;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Queue;
 import java.util.Set;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 public class GraphImpl<N> implements Graph<N> {
 
     private final Map<N, Set<N>> graph;
+    private static final Integer WHITE = 0;
+    private static final Integer BLACK = 1;
+    private static final Integer GRAY = 2;
 
     public GraphImpl() {
-        this.nNodes = 0;
         this.graph = new HashMap<>();
     }
 
@@ -42,12 +46,24 @@ public class GraphImpl<N> implements Graph<N> {
      * Gets one sequence of nodes connecting source to target.
      * 
      * @param source
-     *            the source node
+     *               the source node
      * @param target
-     *            the target node
+     *               the target node
      * @return a sequence of nodes connecting sources and target
      */
-    public List<N> getPath(N source, N target){
-         
+    public List<N> getPath(N source, N target) {
+        final Map<N, Integer> depth = new HashMap<>();
+        final Map<N, N> father = new HashMap<>(this.graph.size());
+        final Map<N, Integer> color = new HashMap<>(this.graph.size());
+        for (final N key : this.graph.keySet()) {
+            depth.put(key, 0);
+            father.put(key, null);
+            color.put(key, WHITE);
+        }
+        depth.put(source, 0);
+        father.put(source, null);
+        color.put(source, GRAY);
+        final Queue<N> coda = new LinkedList<>();
+        // TODO 
     }
 }
