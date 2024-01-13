3cefda8db3218d01b617d938393ebde7c29e7c2c
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-13 12:22:10 +0100

    Esercizio 65: Modificato BreadthFirst.java in modo che implementi PathCalculatorBehavior.java

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/BreadthFirst.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/BreadthFirst.java
@@ -1,10 +1,10 @@
 package it.unibo.generics.graph.impl;
 
-import it.unibo.generics.graph.api.GetPathBehavior;
+import it.unibo.generics.graph.api.PathCalculatorBehavior;
 import it.unibo.generics.graph.api.Graph;
 import java.util.List;
 
-public class BreadthFirst<N> implements GetPathBehavior<N> {
+public class BreadthFirst<N> implements PathCalculatorBehavior<N> {
     
     public List<N> getPath(N source, N target, Graph<N> graph){
         return null;
