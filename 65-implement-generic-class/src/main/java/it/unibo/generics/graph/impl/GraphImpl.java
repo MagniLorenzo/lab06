f9653b67fa037419880189913bc67ac15c472558
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-13 19:28:26 +0100

    Esercizio 65: Modificato GraphImpl.java in modo che sfrutti PathCalculatorBehavior, aggiunto un costruttore che permette di scegliere il comportamento di PathCalculatorBehavior

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -1,23 +1,26 @@
 package it.unibo.generics.graph.impl;
 
 import it.unibo.generics.graph.api.Graph;
+import it.unibo.generics.graph.api.PathCalculatorBehavior;
+
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 
 public class GraphImpl<N> implements Graph<N> {
 
     private final Map<N, Set<N>> graph;
-    private static final Integer WHITE = 0;
-    private static final Integer BLACK = 1;
-    private static final Integer GRAY = 2;
+    private PathCalculatorBehavior<N> pathCalculator;
 
     public GraphImpl() {
         this.graph = new HashMap<>();
+    }
+
+    public GraphImpl(final PathCalculatorBehavior<N> pathCalculatorBehavior) {
+        this();
+        this.pathCalculator = pathCalculatorBehavior;
     }
 
     public void addNode(N node) {
@@ -51,38 +54,7 @@ public class GraphImpl<N> implements Graph<N> {
      * @return a sequence of nodes connecting sources and target
      */
     public List<N> getPath(N source, N target) {
-        final Map<N, Integer> depth = new HashMap<>(this.graph.size());
-        final Map<N, N> father = new HashMap<>(this.graph.size());
-        final Map<N, Integer> color = new HashMap<>(this.graph.size());
-        for (final N key : this.graph.keySet()) {
-            depth.put(key, 0);
-            father.put(key, null);
-            color.put(key, WHITE);
-        }
-        depth.put(source, 0);
-        father.put(source, null);
-        color.put(source, GRAY);
-        final Queue<N> coda = new LinkedList<>();
-        coda.add(source);
-        while (!coda.isEmpty()) {
-            final N actualNode = coda.poll();
-            for (final N node : this.graph.get(actualNode)) {
-                if (color.get(node) == WHITE) {
-                    color.put(node, GRAY);
-                    depth.put(node, depth.get(actualNode) + 1);
-                    father.put(node, actualNode);
-                    coda.add(node);
-                }
-            }
-            color.put(actualNode, BLACK);
-        }
-        final List<N> result = new LinkedList<>();
-        N tmpPointer = target;
-        while (tmpPointer != null) {
-            result.add(0, tmpPointer);
-            tmpPointer = father.get(tmpPointer);
-        }
-        return result;
+        return pathCalculator.getPath(source, target, graph);
     }
 
 }
