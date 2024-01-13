99cb8e162b4b4c7f9222c072cfbfcb65b7ab1c99
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-13 12:24:36 +0100

    Esercizio 65: Creazione AbstractPathCalculator.java con creazione scheletro iniziale

---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/AbstractPathCalculator.java
@@ -0,0 +1,8 @@
+package it.unibo.generics.graph.impl;
+
+import it.unibo.generics.graph.api.PathCalculatorBehavior;
+
+public abstract class AbstractPathCalculator<N> implements PathCalculatorBehavior<N>{
+    
+    
+}
