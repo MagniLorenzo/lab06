b80e6594b3d7405b7570894dbc6c35c04a13e960
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-01-13 14:08:45 +0100

    Esercizio 65: Modificati PathCalculatorBehavior.java e BreadthFirst.java in modo che getPath prenda in input Map<N, Set<N>> come grafo

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/api/PathCalculatorBehavior.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/api/PathCalculatorBehavior.java
@@ -1,9 +1,11 @@
 package it.unibo.generics.graph.api;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface PathCalculatorBehavior<N> {
 
-    public List<N> getPath(N source, N target, Graph<N> graph);
+    List<N> getPath(N source, N target, Map<N, Set<N>> graph);
     
 }
--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/BreadthFirst.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/BreadthFirst.java
@@ -1,12 +1,17 @@
 package it.unibo.generics.graph.impl;
 
 import it.unibo.generics.graph.api.PathCalculatorBehavior;
-import it.unibo.generics.graph.api.Graph;
+
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class BreadthFirst<N> implements PathCalculatorBehavior<N> {
     
-    public List<N> getPath(N source, N target, Graph<N> graph){
+    public List<N> getPath(N source, N target, Map<N, Set<N>> graph){
         return null;
     }
 }
